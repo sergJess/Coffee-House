2da8e330ec74758ed4ddee4271b901e7d45238be
sergJess <serg.fantasy@yandex.ru>
2023-11-26 19:37:33 +0300

    feat: add describe block or hero

---
+++ src/components/button/button.ts
@@ -0,0 +1,15 @@
+import style from './button.css';
+import Node from '../../helpers/node/node';
+type TButton = {
+  href: string;
+  textContent: string;
+};
+export default function Button(buttonDescription: TButton) {
+  const button = Node({
+    tagName: 'button',
+    textContent: buttonDescription.textContent,
+    classList: [style.button],
+  });
+  button.setAttribute('href', buttonDescription.href);
+  return button;
+}
--- src/components/descrip/descrip.ts
+++ src/components/descrip/descrip.ts
@@ -1,7 +1,23 @@
 import style from './descrip.css';
 import Node from '../../helpers/node/node';
+import Button from '../button/button';
 
 export default function Desribe() {
   const container = Node({ tagName: 'div', classList: [style.container] });
+  const button = Button({ href: '#', textContent: 'Menu' });
+  Node({
+    tagName: 'div',
+    classList: [style.title],
+    parent: container,
+    textContent: ' premium coffee at our charming cafe',
+  });
+  Node({
+    tagName: 'p',
+    classList: [style.text],
+    parent: container,
+    textContent:
+      'With its inviting atmosphere and delicious coffee options, the Coffee House Resource is a popular destination for coffee  lovers and those seeking a warm and inviting space to enjoy their favorite beverage.',
+  });
+  container.append(button);
   return container;
 }
