94921c27f299d91f3aa0f53468f1ecb41210e88a
sergJess <serg.fantasy@yandex.ru>
2023-12-17 11:11:17 +0300

    refactor: remove useless code

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,10 @@ import Node from '../helpers/node/node';
 import Header from '../components/header/header';
 import Describe from '../components/descrip/descrip';
 import Slider from '../components/slider/slider';
+
 export default function App() {
   const root = Node({ tagName: 'div', parent: document.body, classList: [style.root] });
   const content = Node({ tagName: 'div' });
   content.append(Describe(), Slider());
   root.append(Header(), content);
-  return root;
 }
